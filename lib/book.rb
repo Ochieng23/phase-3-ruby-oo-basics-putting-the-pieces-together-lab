73585be0a27ac5009edcb3c1bc9ade650edf4620
+ <+>
2023-02-12 15:19:27 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre
+
+ 
+  
+  def initialize(title)
+    @title = title
+    @author = "Agatha Christie"
+    @page_count = page_count
+    @genre =  "Mystery"
+    
+ end
+  
+  def turn_page
+    
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
 
+
+  
